3e644fb7b1a17aa03881b9acc6807ddf852d1faa
V1CeVersaa <285424882@qq.com>
2025-11-07 00:30:14 +0800

    update: finish UMich EECS498 vae and gan

--- UMich-EECS498/assign6/gan.py
+++ UMich-EECS498/assign6/gan.py
@@ -1,8 +1,8 @@
-from __future__ import print_function
-
 import torch
-import torch.utils.data
+
+# import torch.utils.data
 from torch import nn, optim
+from torch.nn import functional as F
 
 NOISE_DIM = 96
 
@@ -23,17 +23,13 @@ def sample_noise(batch_size, noise_dim, dtype=torch.float, device="cpu"):
     - A PyTorch Tensor of shape (batch_size, noise_dim) containing uniform
       random noise in the range (-1, 1).
     """
-    noise = None
     ##############################################################################
     # TODO: Implement sample_noise.                                              #
     ##############################################################################
-    # Replace "pass" statement with your code
-    pass
-
+    noise = torch.rand(batch_size, noise_dim, device=device) * 2 - 1
     ##############################################################################
     #                              END OF YOUR CODE                              #
     ##############################################################################
-
     return noise
 
 
@@ -42,12 +38,17 @@ def discriminator():
     Build and return a PyTorch nn.Sequential model implementing the architecture
     in the notebook.
     """
-    model = None
     ############################################################################
     # TODO: Implement discriminator.                                           #
     ############################################################################
-    # Replace "pass" statement with your code
-    pass
+    model = nn.Sequential(
+        nn.Flatten(),
+        nn.Linear(784, 256),
+        nn.LeakyReLU(0.01),
+        nn.Linear(256, 256),
+        nn.LeakyReLU(0.01),
+        nn.Linear(256, 1),
+    )
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
@@ -59,12 +60,17 @@ def generator(noise_dim=NOISE_DIM):
     Build and return a PyTorch nn.Sequential model implementing the architecture
     in the notebook.
     """
-    model = None
     ############################################################################
     # TODO: Implement generator.                                               #
     ############################################################################
-    # Replace "pass" statement with your code
-    pass
+    model = nn.Sequential(
+        nn.Linear(noise_dim, 1024),
+        nn.ReLU(),
+        nn.Linear(1024, 1024),
+        nn.ReLU(),
+        nn.Linear(1024, 784),
+        nn.Tanh(),
+    )
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
@@ -83,12 +89,12 @@ def discriminator_loss(logits_real, logits_fake):
     Returns:
     - loss: PyTorch Tensor containing (scalar) the loss for the discriminator.
     """
-    loss = None
     ##############################################################################
     # TODO: Implement discriminator_loss.                                        #
     ##############################################################################
-    # Replace "pass" statement with your code
-    pass
+    real_loss = F.binary_cross_entropy_with_logits(logits_real, torch.ones_like(logits_real))
+    fake_loss = F.binary_cross_entropy_with_logits(logits_fake, torch.zeros_like(logits_fake))
+    loss = real_loss + fake_loss
     ##############################################################################
     #                              END OF YOUR CODE                              #
     ##############################################################################
@@ -105,12 +111,10 @@ def generator_loss(logits_fake):
     Returns:
     - loss: PyTorch Tensor containing the (scalar) loss for the generator.
     """
-    loss = None
     ##############################################################################
     # TODO: Implement generator_loss.                                            #
     ##############################################################################
-    # Replace "pass" statement with your code
-    pass
+    loss = F.binary_cross_entropy_with_logits(logits_fake, torch.ones_like(logits_fake))
     ##############################################################################
     #                              END OF YOUR CODE                              #
     ##############################################################################
@@ -128,12 +132,10 @@ def get_optimizer(model):
     Returns:
     - An Adam optimizer for the model with the desired hyperparameters.
     """
-    optimizer = None
     ##############################################################################
     # TODO: Implement optimizer.                                                 #
     ##############################################################################
-    # Replace "pass" statement with your code
-    pass
+    optimizer = optim.Adam(model.parameters(), lr=1e-3, betas=(0.5, 0.999))
     ##############################################################################
     #                              END OF YOUR CODE                              #
     ##############################################################################
@@ -151,12 +153,11 @@ def ls_discriminator_loss(scores_real, scores_fake):
     Outputs:
     - loss: A PyTorch Tensor containing the loss.
     """
-    loss = None
     ##############################################################################
     # TODO: Implement ls_discriminator_loss.                                     #
     ##############################################################################
-    # Replace "pass" statement with your code
-    pass
+    loss = 0.5 * F.mse_loss(scores_real, torch.ones_like(scores_real))
+    loss += 0.5 * F.mse_loss(scores_fake, torch.zeros_like(scores_fake))
     ##############################################################################
     #                              END OF YOUR CODE                              #
     ##############################################################################
@@ -173,12 +174,10 @@ def ls_generator_loss(scores_fake):
     Outputs:
     - loss: A PyTorch Tensor containing the loss.
     """
-    loss = None
     ##############################################################################
     # TODO: Implement ls_generator_loss.                                         #
     ##############################################################################
-    # Replace "pass" statement with your code
-    pass
+    loss = 0.5 * F.mse_loss(scores_fake, torch.ones_like(scores_fake))
     ##############################################################################
     #                              END OF YOUR CODE                              #
     ##############################################################################
@@ -190,12 +189,22 @@ def build_dc_classifier():
     Build and return a PyTorch nn.Sequential model for the DCGAN discriminator
     implementing the architecture in the notebook.
     """
-    model = None
     ############################################################################
     # TODO: Implement build_dc_classifier.                                     #
     ############################################################################
-    # Replace "pass" statement with your code
-    pass
+    model = nn.Sequential(
+        nn.Unflatten(1, (1, 28, 28)),
+        nn.Conv2d(1, 32, 5, 1),
+        nn.LeakyReLU(0.01),
+        nn.MaxPool2d(2, 2),
+        nn.Conv2d(32, 64, 5, 1),
+        nn.LeakyReLU(0.01),
+        nn.MaxPool2d(2, 2),
+        nn.Flatten(),
+        nn.Linear(4 * 4 * 64, 4 * 4 * 64),
+        nn.LeakyReLU(0.01),
+        nn.Linear(4 * 4 * 64, 1),
+    )
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
@@ -208,12 +217,24 @@ def build_dc_generator(noise_dim=NOISE_DIM):
     Build and return a PyTorch nn.Sequential model implementing the DCGAN
     generator using the architecture described in the notebook.
     """
-    model = None
     ############################################################################
     # TODO: Implement build_dc_generator.                                      #
     ############################################################################
-    # Replace "pass" statement with your code
-    pass
+    model = nn.Sequential(
+        nn.Linear(noise_dim, 1024),
+        nn.ReLU(),
+        nn.BatchNorm1d(1024),
+        nn.Linear(1024, 7 * 7 * 128),
+        nn.ReLU(),
+        nn.BatchNorm1d(7 * 7 * 128),
+        nn.Unflatten(1, (128, 7, 7)),
+        nn.ConvTranspose2d(128, 64, 4, 2, 1),
+        nn.ReLU(),
+        nn.BatchNorm2d(64),
+        nn.ConvTranspose2d(64, 1, 4, 2, 1),
+        nn.Tanh(),
+        nn.Flatten(start_dim=1),
+    )
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
